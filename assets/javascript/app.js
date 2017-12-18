a468ae573b39d4d41fa99c12f2611cc45d37cf76
Max Ackerman <max@ackermax.com>
2017-12-18 17:15:33 -0500

    added js and css files and linked them in index.html

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDej6nxcO2oVuS8z7rR9UaDgMj1E0qPbfk",
+    authDomain: "rps-multiplayer-d2a76.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-d2a76.firebaseio.com",
+    projectId: "rps-multiplayer-d2a76",
+    storageBucket: "rps-multiplayer-d2a76.appspot.com",
+    messagingSenderId: "931279302878"
+  };
+  firebase.initializeApp(config);
+  
+  var dBase = firebase.database();
+});
